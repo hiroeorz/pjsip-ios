be9bb13831b0f91c68814338ce1cfdb2d0ff23e3
Shin Hiroe <hiroe.orz@gmail.com>
2014-11-09 12:44:56 +0900

    delete #define setting for using OpenGL

--- build.sh
+++ build.sh
@@ -213,7 +213,6 @@ echo "Creating config.h..."
 echo "#define PJ_CONFIG_IPHONE 1
 #define PJMEDIA_HAS_VIDEO 1
 #define PJMEDIA_HAS_OPENH264_CODEC 1
-#define PJMEDIA_VIDEO_DEV_HAS_IOS_OPENGL 1
 // fix for xcode6
 #import <OpenGLES/ES2/gl.h>
 #import <OpenGLES/ES2/glext.h>
